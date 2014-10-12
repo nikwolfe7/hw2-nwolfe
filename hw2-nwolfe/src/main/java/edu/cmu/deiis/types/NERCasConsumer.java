bbf565aa428eee3d204031345df8be835ad2dae1
nikwolfe7 <nikwolfe7@gmail.com>
2014-10-11 22:15:38 -0400

    Almost done...

--- hw2-nwolfe/src/main/java/edu/cmu/deiis/types/NERCasConsumer.java
+++ hw2-nwolfe/src/main/java/edu/cmu/deiis/types/NERCasConsumer.java
@@ -2,10 +2,12 @@ package edu.cmu.deiis.types;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import org.apache.activemq.transport.stomp.Stomp.Headers.Send;
 import org.apache.uima.UIMA_IllegalArgumentException;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.cas.CAS;
@@ -15,9 +17,16 @@ import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.cas.TOP;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.resource.ResourceProcessException;
+import org.apache.uima.util.ProcessTrace;
 import org.apache.uima.jcas.tcas.Annotation;
 
-public class NERCasConsumer extends CasConsumer_ImplBase {
+/**
+ * @author nwolfe
+ *
+ */
+public class NERCasConsumer<T extends NamedEntityAnnotation> extends CasConsumer_ImplBase {
+
+  private Double confidenceThreshold = -350.0;
 
   private PrintStream writer;
 
@@ -25,11 +34,50 @@ public class NERCasConsumer extends CasConsumer_ImplBase {
 
   private String ABNEngine = "edu.cmu.deiis.types.AbnerAnalysisEngine";
 
-  private HashMap<String, ArrayList<NamedEntityAnnotation>> mergeList;
+  private HashMap<String, ArrayList<AnnotationDataStorage>> mergeList;
+
+  private HashMap<String, ArrayList<AnnotationDataStorage>> HMMlist;
+
+  private HashMap<String, ArrayList<AnnotationDataStorage>> ABNlist;
+
+  private ArrayList<String> alreadySeen;
+
+  /**
+   * 
+   * @author nwolfe This class is to serve as a data storage member the useful data items which have
+   *         been culled from a {@link T};
+   */
+  private class AnnotationDataStorage {
+    Integer begin;
+
+    Integer end;
+
+    @SuppressWarnings("unused")
+    Double confidence;
+
+    String namedEntity;
+
+    String outputString;
+
+    String sentenceId;
+
+    public AnnotationDataStorage(T nea, String sentId) {
+      this.begin = nea.getBegin();
+      this.end = nea.getEnd();
+      this.namedEntity = nea.getNamedEntityString();
+      this.confidence = nea.getConfidence();
+      this.sentenceId = sentId;
+      this.outputString = this.sentenceId + "|" + this.begin.toString() + " " + end.toString()
+              + "|" + namedEntity;
+    }
+  }
 
   @Override
   public void initialize() throws ResourceInitializationException {
-    mergeList = new HashMap<String, ArrayList<NamedEntityAnnotation>>();
+    mergeList = new HashMap<String, ArrayList<AnnotationDataStorage>>();
+    HMMlist = new HashMap<String, ArrayList<AnnotationDataStorage>>();
+    ABNlist = new HashMap<String, ArrayList<AnnotationDataStorage>>();
+
     try {
       this.writer = new PrintStream(new File(getConfigParameterValue(
               UIMATypeEnum.OUTPUT_FILE.getParam()).toString()));
@@ -39,80 +87,89 @@ public class NERCasConsumer extends CasConsumer_ImplBase {
     }
   };
 
-  @Override
   public void destroy() {
     this.writer.flush();
     this.writer.close();
   };
 
-  protected void outputAnnotation(HashMap<String, ArrayList<NamedEntityAnnotation>> model) {
+  /**
+   * @param model
+   *          Given a model of strings mapping to {@link T} objects, print to the output file...
+   */
+  protected void outputAnnotation(HashMap<String, ArrayList<AnnotationDataStorage>> model) {
     for (String key : model.keySet()) {
-      Iterable<NamedEntityAnnotation> iter = model.get(key);
-      for (NamedEntityAnnotation nea : iter) {
-        Integer begin = nea.getBegin();
-        Integer end = nea.getEnd();
-        String namedEntity = nea.getNamedEntityString();
-        String output = key + "|" + begin.toString() + " " + end.toString() + "|" + namedEntity;
-        this.writer.println(output);
+      Iterable<AnnotationDataStorage> iter = model.get(key);
+      for (AnnotationDataStorage nea : iter) {
+        this.writer.println(nea.outputString);
       }
     }
   }
 
-  // Rules for comparing annotations of the same sentence
-  protected void compareAnnotations(String key, ArrayList<NamedEntityAnnotation> item1,
-          ArrayList<NamedEntityAnnotation> item2) {
-    if (item2 != null) {
-      this.mergeList.put(key, item2);
+  @Override
+  public void collectionProcessComplete(ProcessTrace arg0) throws ResourceProcessException,
+          IOException {
+    // TODO Auto-generated method stub
+    super.collectionProcessComplete(arg0);
+    compareAnnotations();
+    outputAnnotation(mergeList);
+  }
+
+  /*
+   * Compare the annotaion lists Simple intersection...
+   * Remove words which are only 1 letter long...
+   */
+  private void compareAnnotations() {
+    for (String key : HMMlist.keySet()) {
+      if (ABNlist.containsKey(key)) {
+        for (AnnotationDataStorage gene : HMMlist.get(key)) {
+          if( gene.namedEntity.length() > 1 ) {
+            addItemToMap(key, gene, mergeList);
+          }
+        }
+      }
     }
   }
 
+  /*
+   * Add items to one of these complex sentence mappings...
+   */
+  private void addItemToMap(String sentenceId, AnnotationDataStorage ds,
+          HashMap<String, ArrayList<AnnotationDataStorage>> map) {
+    if (!map.containsKey(sentenceId)) {
+      map.put(sentenceId, (new ArrayList<AnnotationDataStorage>()));
+    }
+    map.get(sentenceId).add(ds);
+  }
+
+  @SuppressWarnings("unchecked")
   @Override
   public void processCas(CAS aCAS) throws ResourceProcessException {
     try {
       JCas jCas = aCAS.getJCas();
       FSIterator<TOP> fsIterator = jCas.getJFSIndexRepository().getAllIndexedFS(Sentence.type);
-      HashMap<String, ArrayList<NamedEntityAnnotation>> HMMlist = new HashMap<String, ArrayList<NamedEntityAnnotation>>();
-      HashMap<String, ArrayList<NamedEntityAnnotation>> ABNlist = new HashMap<String, ArrayList<NamedEntityAnnotation>>();
-
-      ArrayList<String> sentences = new ArrayList<String>();
 
       if (fsIterator.hasNext()) {
         Sentence sentence = (Sentence) fsIterator.next();
         String sentenceId = sentence.getSentenceId();
-        sentences.add(sentenceId);
 
         // Divide annotations into lists...
-        Iterable<Annotation> iter = jCas.getAnnotationIndex(NamedEntityAnnotation.type);
+        Iterable<Annotation> iter = jCas.getAnnotationIndex(T.type);
         for (Annotation a : iter) {
-          NamedEntityAnnotation nea = (NamedEntityAnnotation) a;
+          // System.out.println(a);
+          T nea = (T) a;
+          AnnotationDataStorage ds = new AnnotationDataStorage(nea, sentenceId);
           String cpid = nea.getCasProcessorId();
           if (cpid.equals(HMMEngine)) {
-            if (!HMMlist.containsKey(sentenceId)) {
-              HMMlist.put(sentenceId, (new ArrayList<NamedEntityAnnotation>()));
-            }
-            HMMlist.get(sentenceId).add(nea);
+            addItemToMap(sentenceId, ds, HMMlist);
           } else if (cpid.equals(ABNEngine)) {
-            if (!ABNlist.containsKey(sentenceId)) {
-              ABNlist.put(sentenceId, (new ArrayList<NamedEntityAnnotation>()));
-            }
-            ABNlist.get(sentenceId).add(nea);
+            addItemToMap(sentenceId, ds, ABNlist);
           }
         }
-
-        // Compare annotations for each sentence....
-        if (ABNlist.containsKey(sentenceId)) {
-          //compareAnnotations(sentenceId, HMMlist.get(sentenceId), ABNlist.get(sentenceId));
-        }
-        
-        // outputAnnotation(HMMlist);
-        outputAnnotation(this.mergeList);
       }
-
     } catch (Exception e) {
       e.printStackTrace();
       throw new AnalysisEngineProcessException();
     }
 
   }
-
 }
